4ce1f45738ae401fc678aa0785ea961a9fa15d0d
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2014-12-08 21:16:51 +0100

    Reworked timeout to work with multiple preconfigured timers at the same time.

--- timeout/timeout.c
+++ timeout/timeout.c
@@ -14,71 +14,126 @@
 
 #include <timeout.h>
 
-#define TIMEOUT_TIMER TIMxy
-#define TIMEOUT_TIMER_RCC RCC_TIMxy
-#define TIMEOUT_TIMER_ISR timxy_isr
-#define TIMEOUT_TIMER_IRQ NVIC_TIMxy_IRQ
-#define TIMEOUT_TIMER_IRQ_PRIO 4
+#include <common.h>
+
+#define TIMER_A_ISR timxy_isr
+#define TIMER_B_ISR timxy_isr
+
+#define IRQ_PRIO (0x03 << 6)
 #define TICKS_PER_MS 48000
 
-bool timeout = true;
-void (*cb_fun)(void *) = NULL;
-void *cb_par = NULL;
+
+static struct timeout_timer {
+	uint32_t base;
+	enum rcc_periph_clken clken;
+	enum rcc_periph_rst rst;
+	uint32_t irq;
+	void (*callback)(void *);
+	void *params;
+	bool periodic;
+	bool running;
+};
+
+static struct timeout_timer conf[] = {
+	// {
+	// 	.base = TIMx,
+	// 	.clken =,
+	// 	.rst = ,
+	// 	.irq =
+	// }
+}
+
+
+static bool valid_timer(enum timeout_timer timer)
+{
+	return timer == TIMEOUT_TIMER_A || timer == TIMEOUT_TIMER_B;
+}
 
 void timeout_init(void)
 {
-	rcc_periph_clock_enable(TIMEOUT_TIMER_RCC);
+	for (int i = 0; i < ARRAY_SIZE(conf); i++ {
 
-	timer_set_mode(TIM6, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-	timer_set_prescaler(TIMEOUT_TIMER, TICKS_PER_MS - 1);
-	timer_enable_irq(TIM6, TIM_DIER_UIE);
+		rcc_periph_clock_enable(conf[i].clken);
 
-	nvic_enable_irq(TIMEOUT_TIMER_IRQ);
-	nvic_set_priority(TIMEOUT_TIMER_IRQ, TIMEOUT_TIMER_IRQ_PRIO);
+		timer_set_mode(conf[i].base, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+		timer_set_prescaler(conf[i].base, TICKS_PER_MS - 1);
+		timer_enable_irq(conf[i].base, TIM_DIER_UIE);
+
+		nvic_enable_irq(conf[i].irq);
+		nvic_set_priority(conf[i].irq, TIMEOUT_TIMER_IRQ_PRIO);
+
+	}
 }
 
-bool timeout_start(uint32_t timeout_ms, bool periodic, void (*callback)(void*), void *cb_params)
+bool timeout_start(enum timeout_timer timer, uint32_t timeout_ms, bool periodic, void (*callback)(void*), void *params)
 {
-	if (TIM_CR1(TIMEOUT_TIMER) & TIM_CR1_CEN) {
+	if (!valid_timer(timer)) {
 		return false;
 	}
 
-	timer_set_period(timeout_ms);
+	if (conf[timer].running) {
+		return false;
+	}
+
+	timer_set_period(conf[timer].base, timeout_ms);
 
 
 	if (periodic) {
-		timer_continuous_mode(TIMEOUT_TIMER);
+		timer_continuous_mode(conf[timer].base);
 	} else {
-		timer_one_shot_mode(TIMEOUT_TIMER);
+		timer_one_shot_mode(conf[timer].base);
 	}
+	conf[timer].periodic = periodic;
 
-	timeout = false;
-	cb_fun = callback;
-	cb_par = cb_params;
+	conf[timer].callback = callback;
+	conf[timer].params = params;
 
-	timer_enable_counter(TIMEOUT_TIMER);
+	conf[timer].running = true;
 
+	timer_enable_counter(conf[timer].base);
 
 	return true;
 }
 
-void timeout_stop(void)
+void timeout_stop(enum timeout_timer timer)
 {
-	timer_disable_counter(TIMEOUT_TIMER);
-}
-
-bool timeout_has_elapsed(void)
-{
-	return timeout;
-}
-
-TIMEOUT_TIMER_ISR(void)
-{
-	timer_clear_flag(TIMEOUT_TIMER, TIM_SR_UIF);
-
-	timeout = true;
-
-	if (cb_fun != NULL) {
-		cb_fun(cb_par);
+	if (!valid_timer(timer)) {
+		return;
 	}
+
+	conf[timer].running = false;
+
+	timer_disable_counter(conf[timer].base);
+}
+
+bool timeout_timer_is_running(enum timeout_timer timer)
+{
+	if (!valid_timer(timer)) {
+		return false;
+	}
+
+	return conf[timer].running;
+}
+
+static inline void isr_handler(enum timeout_timer timer)
+{
+	timer_clear_flag(conf[timer].base, TIM_SR_UIF);
+
+	if (!conf[timer].periodic) {
+		conf[timer].running = false;
+	}
+
+	if (conf[timer].callback != NULL) {
+		conf[timer].callback(conf[timer].params);
+	}
+}
+
+TIMER_A_ISR(void)
+{
+	isr_handler(TIMEOUT_TIMER_A);
+}
+
+TIMER_B_ISR(void)
+{
+	isr_handler(TIMEOUT_TIMER_B);
 }
--- timeout/timeout.h
+++ timeout/timeout.h
@@ -11,9 +11,14 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+enum timeout_timer {
+	TIMEOUT_TIMER_A = 0,
+	TIMEOUT_TIMER_B,
+};
+
 void timeout_init(void);
-bool timeout_start(uint32_t timeout_ms, bool periodic, void (*callback)(void*), void *cb_params);
-void timeout_stop(void);
-bool timeout_has_elapsed(void);
+bool timeout_start(enum timeout_timer timer, uint32_t timeout_ms, bool periodic, void (*callback)(void*), void *params);
+void timeout_stop(enum timeout_timer timer);
+bool timeout_timer_is_running(enum timeout_timer timer);
 
 #endif /* TIMEOUT_H_ */
