2b1b8f0dcb06ce65c0af2ee911b8e916a138d561
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2014-12-22 00:07:54 +0100

    Got timers to work.
    Added stupid hack for stopping the callback function being called immediately after timer start. Seems like the timer sets the update flag and the capture compare flag automatically on timer enable.

--- timeout/timeout.c
+++ timeout/timeout.c
@@ -6,24 +6,25 @@
  */
 
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
 
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/timer.h>
-#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/dispatch/nvic.h>
 
 #include <timeout.h>
-
 #include <common.h>
 
-#define TIMER_A_ISR timxy_isr
-#define TIMER_B_ISR timxy_isr
+
+#define TIMER_A_ISR tim16_isr
+#define TIMER_B_ISR tim17_isr
 
 #define IRQ_PRIO (0x03 << 6)
 #define TICKS_PER_MS 48000
 
 
-static struct timeout_timer {
+struct timer_conf {
 	uint32_t base;
 	enum rcc_periph_clken clken;
 	enum rcc_periph_rst rst;
@@ -32,16 +33,24 @@ static struct timeout_timer {
 	void *params;
 	bool periodic;
 	bool running;
+
+	bool first_it;
 };
 
-static struct timeout_timer conf[] = {
-	// {
-	// 	.base = TIMx,
-	// 	.clken =,
-	// 	.rst = ,
-	// 	.irq =
-	// }
-}
+static struct timer_conf conf[] = {
+	 {
+	 	.base = TIM16,
+	 	.clken = RCC_TIM16,
+	 	.rst = RST_TIM16,
+	 	.irq = NVIC_TIM16_IRQ
+	 },
+	 {
+	 	.base = TIM17,
+	 	.clken = RCC_TIM17,
+	 	.rst = RST_TIM17,
+	 	.irq = NVIC_TIM17_IRQ
+	 }
+};
 
 
 static bool valid_timer(enum timeout_timer timer)
@@ -51,17 +60,21 @@ static bool valid_timer(enum timeout_timer timer)
 
 void timeout_init(void)
 {
-	for (int i = 0; i < ARRAY_SIZE(conf); i++ {
+	for (int i = 0; i < ARRAY_SIZE(conf); i++) {
 
 		rcc_periph_clock_enable(conf[i].clken);
 
+		rcc_periph_reset_pulse(conf[i].rst);
+
 		timer_set_mode(conf[i].base, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
 		timer_set_prescaler(conf[i].base, TICKS_PER_MS - 1);
+
+		timer_update_on_overflow(conf[i].base);
+
 		timer_enable_irq(conf[i].base, TIM_DIER_UIE);
 
 		nvic_enable_irq(conf[i].irq);
-		nvic_set_priority(conf[i].irq, TIMEOUT_TIMER_IRQ_PRIO);
-
+		nvic_set_priority(conf[i].irq, IRQ_PRIO);
 	}
 }
 
@@ -75,20 +88,17 @@ bool timeout_start(enum timeout_timer timer, uint32_t timeout_ms, bool periodic,
 		return false;
 	}
 
+	timer_set_counter(conf[timer].base, 1);
 	timer_set_period(conf[timer].base, timeout_ms);
 
-
-	if (periodic) {
-		timer_continuous_mode(conf[timer].base);
-	} else {
-		timer_one_shot_mode(conf[timer].base);
-	}
 	conf[timer].periodic = periodic;
 
 	conf[timer].callback = callback;
 	conf[timer].params = params;
 
 	conf[timer].running = true;
+
+	conf[timer].first_it = true;
 
 	timer_enable_counter(conf[timer].base);
 
@@ -119,21 +129,26 @@ static inline void isr_handler(enum timeout_timer timer)
 {
 	timer_clear_flag(conf[timer].base, TIM_SR_UIF);
 
-	if (!conf[timer].periodic) {
-		conf[timer].running = false;
-	}
+	if (!conf[timer].first_it) {
+		if (!conf[timer].periodic) {
+			conf[timer].running = false;
+			timer_disable_counter(conf[timer].base);
+		}
 
-	if (conf[timer].callback != NULL) {
-		conf[timer].callback(conf[timer].params);
+		if (conf[timer].callback != NULL) {
+			conf[timer].callback(conf[timer].params);
+		}
+	} else {
+		conf[timer].first_it = false;
 	}
 }
 
-TIMER_A_ISR(void)
+void TIMER_A_ISR(void)
 {
 	isr_handler(TIMEOUT_TIMER_A);
 }
 
-TIMER_B_ISR(void)
+void TIMER_B_ISR(void)
 {
 	isr_handler(TIMEOUT_TIMER_B);
 }
