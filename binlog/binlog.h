8a96697b1c788a8ca9c301757535a3e9b9574c5c
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2015-04-07 23:27:33 +0200

    Forgotten ;

--- binlog/binlog.h
+++ binlog/binlog.h
@@ -11,7 +11,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-void binlog_init(bool (*send_func)(uint8_t ch), void (*err_func)())
+void binlog_init(bool (*send_func)(uint8_t ch), void (*err_func)());
 void binlog_log(uint8_t log_code, uint8_t *arg_buf, uint8_t arg_buf_len);
 
 #endif /* BINLOG_H_ */
