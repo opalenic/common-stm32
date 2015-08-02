1b98beb0dd85a510fa0f8c2dd3b41efd3cfad468
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2015-08-02 22:40:39 +0200

    Reworked CLI to use simple arrays instead of linked lists and use MourOS.

--- cli/cli.c
+++ cli/cli.c
@@ -6,60 +6,22 @@
  */
 
 
+#include <stddef.h> // NULL
+#include <string.h> // strcmp, strtok
+#include <stdio.h>  // sniprintf
 
-#include <string.h>
-#include <limits.h>
+#include <libopencm3/cm3/assert.h> // For cm3_assert()
+#include <mouros/common.h>         // ARRAY_SIZE()
+#include <mouros/tasks.h>          // os_task_sleep() & os_task_yield()
 
-#include "cli.h"
-#include "comm.h"
-#include "common.h"
+#include "cli.h" // Public function declarations.
 
-#define MAX_CL_LENGTH 40
 
-#define MAX_COMMANDS 10
 
+#define MAX_CMD_LINE_LENGTH 100
+#define MAX_CMDS 30
+#define MAX_CMD_ARGS 10
 #define MAX_ESC_SEQ_LENGTH 5
-
-// -----------------------------------------------------------------------------
-
-enum char_type {
-	PRINTABLE,
-	DELETE,
-	BACKSPACE,
-	ENTER,
-	ESCAPE,
-	UNKNOWN
-};
-
-
-LIST_HEAD(cli_char_list_head, character);
-struct cli_char_list_head free_char_list = LIST_HEAD_INITIALIZER();
-struct cli_char_list_head command_char_list = LIST_HEAD_INITIALIZER();
-
-struct character char_storage[MAX_CL_LENGTH];
-
-struct character *cursor_after = NULL;
-
-// -----------------------------------------------------------------------------
-
-
-
-// -----------------------------------------------------------------------------
-
-struct command {
-	LIST_ENTRY(command) list_el;
-	const char *name;
-	void (*cmd_func)(struct character *first_opt);
-};
-
-LIST_HEAD(cmd_list_head, command);
-struct cmd_list_head free_cmd_list = LIST_HEAD_INITIALIZER();
-struct cmd_list_head cmd_list = LIST_HEAD_INITIALIZER();
-
-struct command command_storage[MAX_COMMANDS];
-
-// -----------------------------------------------------------------------------
-
 
 enum term_color {
 	BLACK = '0',
@@ -72,58 +34,79 @@ enum term_color {
 	WHITE = '7'
 };
 
+enum char_type {
+	PRINTABLE,
+	DELETE,
+	BACKSPACE,
+	ENTER,
+	ESCAPE,
+	UNKNOWN
+};
+
+struct command {
+	const char *name;
+	cli_cmd_main_function cmd_func;
+};
 
 
-static struct character *char_take(void);
-static void char_give(struct character *ch);
-static struct command *cmd_take(void);
-static void cmd_give(struct command *cmd);
+static char cmd_line[MAX_CMD_LINE_LENGTH + 1]; // Extra space for terminating '\0'
+static uint8_t cmd_line_len = 0;
+static uint8_t cursor_pos = 0;
 
 
-static enum char_type get_char_type(uint8_t ch);
+static struct command commands[MAX_CMDS];
+static uint8_t num_commands = 0;
+
+
+static buffer_t *rx = NULL;
+static buffer_t *tx = NULL;
+
+
+static enum char_type get_char_type(char ch);
 static void set_term_color(enum term_color color);
 
 static void process_left_key(void);
 static void process_right_key(void);
 
-static void process_char_insert(uint8_t ch);
+static void process_char_insert(char ch);
 static void process_char_delete(void);
 static void process_char_backspace(void);
 static void process_enter_pressed(void);
 static void process_escape_seq(void);
+
 static void output_prompt(void);
 
+static int execute_command(void);
 
-static void execute_command(void);
-
-static void help_command(struct character *first_opt);
-
+static void help_command(void);
 
 
-void cli_init(void)
+
+void cli_init(buffer_t *rx_buf, buffer_t *tx_buf)
 {
-	// initialize the list of free chars
-	for (uint16_t i = 0; i < MAX_CL_LENGTH; i++) {
-		LIST_INSERT_HEAD(&free_char_list, &char_storage[i], list_el);
-	}
-
-	// initialize the list of free command structs
-	for (uint16_t i = 0; i < MAX_COMMANDS; i++) {
-		LIST_INSERT_HEAD(&free_cmd_list, &command_storage[i], list_el);
-	}
+	rx = rx_buf;
+	tx = tx_buf;
 }
 
 
-void cli_loop(void)
+void cli_loop(uint32_t sleep_ticks)
 {
+	cm3_assert(rx != NULL && tx != NULL);
+
 	set_term_color(BLUE);
 
 	while (true) {
-		uint8_t new_ch = comm_read_ch_blocking();
+		output_prompt();
 
-		switch (get_char_type(new_ch)) {
+		char ch;
+		if (!os_buffer_read_ch(rx, (uint8_t *) &ch)) {
+			os_task_sleep(sleep_ticks);
+			continue;
+		}
+
+		switch (get_char_type(ch)) {
 		case PRINTABLE:
-			process_char_insert(new_ch);
+			process_char_insert(ch);
 
 			break;
 		case DELETE:
@@ -145,44 +128,33 @@ void cli_loop(void)
 		default:
 			break;
 		}
-
-		output_prompt();
 	}
 }
 
-bool cli_register_command(const char *name, void (*cmd_func)(struct character *first_opt))
+bool cli_register_command(const char *name, cli_cmd_main_function cmd_func)
 {
-	struct command *cmd = LIST_FIRST(&cmd_list);
-	while (cmd != NULL) {
-		if (strcmp(name, cmd->name) == 0) {
-			return false;
-		}
-
-		cmd = LIST_NEXT(cmd, list_el);
-	}
-
-	cmd = cmd_take();
-
-	if (cmd == NULL) {
+	if (num_commands >= MAX_CMDS) {
 		return false;
 	}
 
-	cmd->name = name;
-	cmd->cmd_func = cmd_func;
+	commands[num_commands].name = name;
+	commands[num_commands].cmd_func = cmd_func;
 
-	LIST_INSERT_HEAD(&cmd_list, cmd, list_el);
+	num_commands++;
 
 	return true;
 }
 
 bool cli_deregister_command(const char *name)
 {
-	struct command *cmd = LIST_FIRST(&cmd_list);
+	for (uint8_t i = 0; i < num_commands; i++) {
+		if (strcmp(commands[i].name, name) == 0) {
+			for (uint8_t j = i; j < num_commands - 1; j++) {
+				commands[j] = commands[j + 1];
+			}
 
-	while (cmd != NULL) {
-		if (strcmp(cmd->name, name) == 0) {
-			LIST_REMOVE(cmd, list_el);
-			cmd_give(cmd);
+			num_commands--;
+
 			return true;
 		}
 	}
@@ -190,154 +162,11 @@ bool cli_deregister_command(const char *name)
 	return false;
 }
 
-int8_t cli_match_tok(struct character *first, const char* str)
-{
-	struct character *cl_char = first;
-
-	uint8_t str_pos = 0;
-	uint8_t ch = str[str_pos++];
-
-	// Iterate until end of either string or until no match.
-	while (cl_char != NULL && ch != '\0' && cl_char->ch == ch) {
-
-		cl_char = LIST_NEXT(cl_char, list_el);
-		ch = str[str_pos++];
-	}
-
-	// A successful match is when we reached the end of 'str' and are either
-	// at the end of the string headed by 'first', or the next character is
-	// a space (everything after a space are options).
-	if (ch == '\0' && (cl_char == NULL || cl_char->ch == ' ')) {
-		return 0;
-
-	// This means, that we got to the end of 'str', but there are still
-	// chars left in 'first'.
-	} else if (ch == '\0') {
-		return 1;
-	// We didn't get to the end of 'str'.
-	} else {
-		return -1;
-	}
-}
-
-bool cli_parse_tok_int16(struct character *first, int16_t *res)
-{
-//	if (first == NULL) {
-//		return false;
-//	}
-//
-//	struct character *curr = first;
-//	bool is_negative = false;
-//	if (first == '-') {
-//		is_negative = true;
-//		curr = LIST_NEXT(first, list_el);
-//	}
-//
-//	uint16_t res;
-//	cli_parse_tok_uint16(curr, res);
-
-	return false;
-}
-
-bool cli_parse_tok_uint16(struct character *first, uint16_t *res)
-{
-	if (first == NULL) {
-		return false;
-	}
-
-	uint32_t tmp = 0;
-
-	struct character *curr = first;
-
-	while (curr != NULL) {
-		if (tmp > UINT16_MAX) {
-			return false;
-		}
-
-		if (curr->ch < '0' || curr->ch > '9') {
-			return false;
-		}
-
-		uint8_t digit = curr->ch - '0';
-		tmp = tmp * 10 + digit;
-
-		curr = LIST_NEXT(curr, list_el);
-	}
-
-	*res = tmp;
-	return true;
-}
-
-
-struct character *cli_next_tok(struct character *first)
-{
-	struct character *ch = first;
-
-	while (ch != NULL && ch->ch != ' ') {
-
-		ch = LIST_NEXT(ch, list_el);
-	}
-
-	if (ch != NULL) {
-		do {
-			ch = LIST_NEXT(ch, list_el);
-		} while (ch != NULL && ch->ch == ' ');
-	}
-
-	return ch;
-
-}
-
-
-
-
-
-// ----------------------------------------------------------
-// MEMORY MANAGEMENT
-// ----------------------------------------------------------
-static struct character *char_take(void)
-{
-	struct character *ch = LIST_FIRST(&free_char_list);
-
-	if (ch != NULL) {
-		LIST_REMOVE(ch, list_el);
-	}
-	return ch;
-}
-
-static void char_give(struct character *ch)
-{
-	if (ch == NULL) {
-		return;
-	}
-
-	LIST_INSERT_HEAD(&free_char_list, ch, list_el);
-}
-
-static struct command *cmd_take(void)
-{
-	struct command *cmd = LIST_FIRST(&free_cmd_list);
-
-	if (cmd != NULL) {
-		LIST_REMOVE(cmd, list_el);
-	}
-	return cmd;
-}
-
-static void cmd_give(struct command *cmd)
-{
-	if (cmd == NULL) {
-		return;
-	}
-
-	LIST_INSERT_HEAD(&free_cmd_list, cmd, list_el);
-}
-
 
 // ----------------------------------------------------------
 // CLI I/O
 // ----------------------------------------------------------
-static enum char_type get_char_type(uint8_t ch)
+static enum char_type get_char_type(char ch)
 {
 	if ((ch >= 32 && ch <= 126) || ch == '\t') {
 		return PRINTABLE;
@@ -356,71 +185,75 @@ static enum char_type get_char_type(uint8_t ch)
 
 static void set_term_color(enum term_color color)
 {
-	comm_send_str("\e[3");
-	comm_send_ch(color);
-	comm_send_ch('m');
+	os_buffer_write_str(tx, "\e[3");
+	os_buffer_write_byte(tx, color);
+	os_buffer_write_byte(tx, 'm');
 }
 
 static void process_left_key(void)
 {
-	if (cursor_after != NULL) {
-		cursor_after = LIST_PREV(cursor_after, &command_char_list, character, list_el);
+	if (cursor_pos == 0) {
+		return;
 	}
+
+	cursor_pos--;
 }
 
 static void process_right_key(void)
 {
-	struct character *next = LIST_NEXT(cursor_after, list_el);
-	if (next != NULL) {
-		cursor_after = next;
-	}
-}
-
-static void process_char_insert(uint8_t ch)
-{
-	struct character *cl_char = char_take();
-	if (cl_char == NULL) {
+	if (cursor_pos >= cmd_line_len) {
 		return;
 	}
 
-	cl_char->ch = ch;
+	cursor_pos++;
+}
 
-	if (cursor_after != NULL) {
-		LIST_INSERT_AFTER(cursor_after, cl_char, list_el);
-	} else {
-		LIST_INSERT_HEAD(&command_char_list, cl_char, list_el);
+static void process_char_insert(char ch)
+{
+	if (cmd_line_len >= MAX_CMD_LINE_LENGTH) {
+		return;
 	}
 
-	cursor_after = cl_char;
+	for (uint8_t i = cursor_pos; i < cmd_line_len; i++) {
+		cmd_line[i + 1] = cmd_line[i];
+	}
 
+	cmd_line[cursor_pos] = ch;
+
+	cursor_pos++;
+	cmd_line_len++;
 }
 
 static void process_char_delete(void)
 {
-	struct character *cl_char = LIST_NEXT(cursor_after, list_el);
-	if (cl_char != NULL) {
-		LIST_REMOVE(cl_char, list_el);
-		char_give(cl_char);
+	if (cursor_pos == cmd_line_len) {
+		return;
 	}
 
+	for (uint8_t i = cursor_pos; i < cmd_line_len - 1; i++) {
+		cmd_line[i] = cmd_line[i + 1];
+	}
+
+	cmd_line_len--;
 }
 
 static void process_char_backspace(void)
 {
-	if (cursor_after != NULL) {
-		struct character *cl_char = LIST_PREV(cursor_after,
-				&command_char_list, character, list_el);
-
-		LIST_REMOVE(cursor_after, list_el);
-		char_give(cursor_after);
-
-		cursor_after = cl_char;
+	if (cursor_pos == 0) {
+		return;
 	}
+
+	for (uint8_t i = cursor_pos; i < cmd_line_len; i++) {
+		cmd_line[i - 1] = cmd_line[i];
+	}
+
+	cursor_pos--;
+	cmd_line_len--;
 }
 
 static void process_enter_pressed(void)
 {
-	comm_send_str("\r\n");
+	os_buffer_write_str(tx, "\r\n");
 
 	set_term_color(YELLOW);
 
@@ -428,21 +261,12 @@ static void process_enter_pressed(void)
 
 	set_term_color(BLUE);
 
-	struct character *cl_char = LIST_FIRST(&command_char_list);
-	while (cl_char != NULL) {
-		struct character *next = LIST_NEXT(cl_char, list_el);
-		LIST_REMOVE(cl_char, list_el);
-		char_give(cl_char);
-
-		cl_char = next;
-	}
-
-	cursor_after = NULL;
+	cmd_line_len = 0;
+	cursor_pos = 0;
 }
 
 static void process_escape_seq(void)
 {
-
 	struct escape_sequence {
 		const char *code;
 		void (*action)(void);
@@ -464,12 +288,16 @@ static void process_escape_seq(void)
 
 	uint8_t possible_matches = ARRAY_SIZE(sequences);
 
-	uint8_t esc_chars[MAX_ESC_SEQ_LENGTH + 1];
+	char esc_chars[MAX_ESC_SEQ_LENGTH + 1];
 	uint8_t esc_char_num = 0;
 
 	while (possible_matches != 0 && esc_char_num < MAX_ESC_SEQ_LENGTH) {
 
-		uint8_t new_ch = comm_read_ch_blocking();
+		char new_ch;
+		while (!os_buffer_read_ch(rx, (uint8_t *) &new_ch)) {
+			os_task_yield();
+		}
+
 		if (get_char_type(new_ch) != PRINTABLE) {
 			break;
 		}
@@ -484,12 +312,12 @@ static void process_escape_seq(void)
 				continue;
 			}
 
-			int8_t match = strcmp(esc_chars, sequences[i].code);
+			int match = strcmp(esc_chars, sequences[i].code);
 
 			if (match == 0) {
 				sequences[i].action();
-
 				return;
+
 			} else if (match > 0) {
 				sequences[i].no_match = true;
 				possible_matches--;
@@ -504,77 +332,72 @@ static void process_escape_seq(void)
 
 static void output_prompt(void)
 {
-	comm_send_str("\e[2K"); // clears current line
-	comm_send_str("\r -> ");
+	os_buffer_write_str(tx, "\e[2K"); // clears the current line
+	os_buffer_write_str(tx, "\r -> ");
 
-	uint16_t total_len = 0;
-	uint16_t cursor_pos = 0;
-	// output the chars in command_char_list
-	for (struct character *curr = LIST_FIRST(&command_char_list);
-			curr != NULL;
-			curr = LIST_NEXT(curr, list_el)) {
-		comm_send_ch(curr->ch);
 
-		total_len++;
+	os_buffer_write(tx, (uint8_t *) cmd_line, cmd_line_len);
 
-		if (cursor_after == curr) {
-			cursor_pos = total_len;
-		}
-
-	}
-
-	uint16_t shift = total_len - cursor_pos;
+	int shift = cmd_line_len - cursor_pos;
 
 	if (shift > 0) {
-		comm_send_str("\e[");
-		comm_send_num_u(shift);
-		comm_send_ch('D');
+		char shift_buf[12];
+		int buf_len = sniprintf(shift_buf, 12, "\e[%dD", shift);
+
+		if (buf_len > 0 && buf_len < 12) {
+			os_buffer_write(tx, (uint8_t *) shift_buf, (uint32_t) buf_len);
+		}
 	}
 }
 
 
-// ----------------------------------------------------------
-// Command processing
-// ----------------------------------------------------------
-static void execute_command(void)
+static int execute_command(void)
 {
-	struct character *first_char = LIST_FIRST(&command_char_list);
-	struct character *first_opt = cli_next_tok(first_char);
+	int argc = 1;
+	char *argv[MAX_CMD_ARGS];
+	char *saveptr;
 
-	struct command *cmd = LIST_FIRST(&cmd_list);
+	cmd_line[cmd_line_len] = '\0';
 
-	while (cmd != NULL) {
+	argv[0] = strtok_r(cmd_line, " \t", &saveptr);
 
-		if (cli_match_tok(first_char, cmd->name) == 0) {
-			cmd->cmd_func(first_opt);
-			return;
+	if (argv[0] != NULL) {
+		for (uint8_t i = 0; i < num_commands; i++) {
+			if (strcmp(argv[0], commands[i].name) == 0) {
+
+				char *token = strtok_r(NULL, " \t", &saveptr);
+
+				while(token != NULL && argc != MAX_CMD_ARGS) {
+					argv[argc++] = token;
+					token = strtok_r(NULL, " \t", &saveptr);
+				}
+
+				return commands[i].cmd_func(rx, tx, argc, argv);
+			}
 		}
 
-		cmd = LIST_NEXT(cmd, list_el);
+		if (strcmp(argv[0], "help") == 0) {
+			help_command();
+			return -1;
+		}
 	}
 
-	if (cli_match_tok(first_char, "help") == 0) {
-		help_command(first_opt);
-		return;
-	}
+	os_buffer_write_str(tx, "Command not found.\r\n");
 
-	comm_send_str("Command not found.\r\n");
+	return -1;
 }
 
 
-static void help_command(struct character *first_opt)
+static void help_command(void)
 {
-	comm_send_str("The currently registered commands are:\r\n");
+	os_buffer_write_str(tx, "The currently registered commands are:\r\n");
 
-	struct command *cmd = LIST_FIRST(&cmd_list);
-	while (cmd != NULL) {
+	for (uint8_t i = 0; i < num_commands; i++) {
 
-		comm_send_ch('\t');
-		comm_send_str(cmd->name);
-		comm_send_str("\r\n");
-
-		cmd = LIST_NEXT(cmd, list_el);
+		os_buffer_write_byte(tx, '\t');
+		os_buffer_write_str(tx, commands[i].name);
+		os_buffer_write_str(tx, "\r\n");
 	}
 
-	comm_send_str("\r\n");
+	os_buffer_write_str(tx, "\r\n");
 }
--- cli/cli.h
+++ cli/cli.h
@@ -1,34 +1,26 @@
-/*
- * cli.h
+/**
+ * @file
  *
- *  Created on: 7. 9. 2014
- *      Author: ondra
+ * simple command line interface for MourOS.
+ *
  */
 
-#ifndef CLI_H_
-#define CLI_H_
+#ifndef MOUROS_CLI_H_
+#define MOUROS_CLI_H_
 
 #include <stdbool.h>
 #include <stdint.h>
-#include <sys/queue.h>
 
+#include <mouros/buffer.h>
 
-struct character {
-	LIST_ENTRY(character) list_el;
-	uint8_t ch;
-};
+typedef int (*cli_cmd_main_function)(buffer_t *rx_buf, buffer_t *tx_buf, int argc, char *argv[]);
 
-void cli_init(void);
-void cli_loop(void);
+void cli_init(buffer_t *rx_buf, buffer_t *tx_buf);
 
-bool cli_register_command(const char *name, void (*cmd_func)(struct character *first_opt));
+void cli_loop(uint32_t sleep_ticks);
+
+bool cli_register_command(const char *name, cli_cmd_main_function cmd_func);
 bool cli_deregister_command(const char *name);
 
-int8_t cli_match_tok(struct character *first, const char* str);
-bool cli_parse_tok_int16(struct character *first, int16_t *res);
-bool cli_parse_tok_uint16(struct character *first, uint16_t *res);
-struct character *cli_next_tok(struct character *first);
 
-
-
-#endif /* CLI_H_ */
+#endif /* MOUROS_CLI_H_ */
